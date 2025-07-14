97a4dcd8e3703a933705a66e0722e76195f6b060
guinjung <38446773+guinjung@users.noreply.github.com>
2025-07-14 18:02:45 +0900

    경로변경 및 내용확인

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,14 @@
+[guinjung@localhost Music]$ cd /home/guinjung/Music
+[guinjung@localhost Music]$ ls
+[guinjung@localhost Music]$ pwd
+/home/guinjung/Music
+[guinjung@localhost Music]$ cd ..
+[guinjung@localhost ~]$ cd Picture
+bash: cd: Picture: No such file or directory
+[guinjung@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[guinjung@localhost ~]$ cd Pictures
+[guinjung@localhost Pictures]$ ls
+[guinjung@localhost Pictures]$ pwd
+/home/guinjung/Pictures
+[guinjung@localhost Pictures]$ 
