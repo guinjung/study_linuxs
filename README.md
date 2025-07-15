b222f65444239128fed0b5c8a67c12b2256d6d8b
guinjung <38446773+guinjung@users.noreply.github.com>
2025-07-15 12:23:09 +0900

    두번째 올리기

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # study_linuxs
+## powershell (GUI & CLI)
+연습 글을 올립니다
+- 첫번째
+- 두번째
+
+[구글](https://www.google.com)
+
+[안내글](codes/10_powershells.sh)
+
---
+++ codes/10_powershells.sh
@@ -0,0 +1,36 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+새로운 크로스 플랫폼 PowerShell 사용 https://aka.ms/pscore6
+
+PS C:\Users\Administrator> cd C:\
+PS C:\> cd C:\Develops
+PS C:\Develops> dir
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:59                Downloads
+d-----      2025-07-14   오후 5:04                VMs
+
+
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:59                Downloads
+d-----      2025-07-14   오후 5:04                VMs
